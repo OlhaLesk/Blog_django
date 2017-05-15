5cdb34003b729a632405a4981b342c4b22a52f38
Olha <olhalesk@gmail.com>
2017-05-15 22:42:04 +0300

    Possibilities to make drafts and publishing them are added

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,8 @@ from django.utils import timezone
 
 
 class Post(models.Model):
+    class Meta:
+        db_table = "post"
     author = models.ForeignKey('auth.User')
     title = models.CharField(max_length=200)
     text = models.TextField()
